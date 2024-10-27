8fd2a6d7cf9815018fc81b58c5cd84c8f2f2f07f
Marina Siqueira <marihersiq@gmail.com>
2024-10-27 14:58:18 -0300

    Adicionando classe D com métodos MD1 e MD2

---
+++ D.java
@@ -0,0 +1,21 @@
+public class D {
+    private int D1;
+    private int D2;
+
+    public int MD1(){ 
+        int soma = D1 + D2;
+        return soma;
+    }
+
+    public int MD2(){ 
+        if (D1 > D2){
+            return D1;
+        }
+        else if (D2 > D1) {
+            return D2;
+        }
+        else {
+            return 0;
+        }
+    }
+}
